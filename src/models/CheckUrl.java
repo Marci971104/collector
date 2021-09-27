9721a6b77cb1b5e27b36bcf289cd3c647ae0930c
Rohrbacher Marcell <rohrbacher.marcell97@gmail.com>
2021-09-27 12:02:26 +0200

    Check Urd.java kesz

--- src/models/CheckUrl.java
+++ src/models/CheckUrl.java
@@ -18,9 +18,10 @@ public class CheckUrl {
     // TODO: beírt URL ellenőrző
     public void check(String url) {
 
-    }//URL ellenőrzés vége
+    }
     // TODO: ha nincs https:// akkor kiegészítjük
     public void expandProtocol() {
+        
 
-    }//URL kiegészítés vége
+    }
 }
